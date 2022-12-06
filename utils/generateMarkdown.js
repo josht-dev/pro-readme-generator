d88502da3247d1068576a7a99652ca64ef99f8c6
Joshua Taylor <toasterrage@gmail.com>
2022-12-06 16:45:49 -0700

    Updated license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
-  // https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>?style=for-the-badge&logo=appveyor
-
-
+  return (license) ? `https://img.shields.io/badge/license-${license}-brightgreen?style=for-the-badge&logo=appveyor` : '';
 }
 
 // TODO: Create a function that returns the license link
