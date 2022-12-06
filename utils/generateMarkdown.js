caefc62a0c77cb6ffab40e5abb06916768b1a0d1
Joshua Taylor <toasterrage@gmail.com>
2022-12-06 15:22:34 -0700

    Added license badge url to use

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  // https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>?style=for-the-badge&logo=appveyor
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
