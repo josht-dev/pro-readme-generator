e1c16d81a01f3157f9e29603accfc34ecd60615f
Joshua Taylor <toasterrage@gmail.com>
2022-12-06 12:54:17 -0700

    Added initial user questions'

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Enter the project title: ", 
+    "Add a table of contents? Y/N ", 
+    "Enter installation instructions with a ';' between each step: ", 
+    "Add usage instructions: ", 
+    "What license does your project use? ", 
+    "Add optional sections; badges, features, tests, and how to contribute? Y/N ", 
+    "Add badges...", 
+    "List Features separated by ';' between each feature: ", 
+    "Add instructions on how others can contribute: ", 
+    "Add the tests section? Y/N "
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
