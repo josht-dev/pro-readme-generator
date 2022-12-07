3316cae1a54a1d5fa0806bd8703362d90dce63c7
Joshua Taylor <toasterrage@gmail.com>
2022-12-07 00:47:56 -0700

    Updated questions array to inquire array obj format

--- index.js
+++ index.js
@@ -5,16 +5,58 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "Enter the project title: ", 
-    "Enter a project description: ",
-    "Enter installation instructions with a ';' between each step: ", 
-    "Enter usage instructions: ", 
-    //"List Features separated by ';' between each feature: ", 
-    "Enter instructions on how others can contribute: ",
-    "Enter test instructions: ",
-    "Choose a license to use: ",
-    "Enter your GitHub username: ",
-    "Enter your email address: "
+    {
+        type: 'input',
+        message: "Enter the project title: ",
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: "Enter a project description: ",
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: "Enter installation instructions with a ';' between each step: ",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Enter usage instructions: ",
+        name: 'usage'
+    },
+    /*{
+        type: 'input',
+        message: "List Features separated by ';' between each feature: ",
+        name: 'features'
+    },*/
+    {
+        type: 'input',
+        message: "Enter instructions on how others can contribute: ",
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: "Enter test instructions: ",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license to use: ",
+        name: 'license',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public 2.0', 'Apache 2.0', 'MIT', 'Boost Software 1.0', 'Unlicense'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        message: "Enter your GitHub username: ",
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: "Enter your email address: ",
+        name: 'email'
+    }
 ];
 
 // Create a function to write README file
