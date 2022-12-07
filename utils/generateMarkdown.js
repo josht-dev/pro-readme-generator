402c13c2f08549a20539d76b60bc5fbef8b894e2
Joshua Taylor <toasterrage@gmail.com>
2022-12-07 10:22:09 -0700

    Updated license code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,33 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Create a function that returns a license badge based on which license is passed in
+// STARTER COMMENT: If there is no license, return an empty string
+/* Validation is present in inquirer call, user selects from a list to proceed,
+  function is never called without a license present*/
 function renderLicenseBadge(license) {
   return (license) ? `https://img.shields.io/badge/license-${license}-brightgreen?style=for-the-badge&logo=appveyor` : '';
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Create a function that returns the license link
+// STARTER COMMENT: If there is no license, return an empty string
+/* Validation is present in inquirer call, user selects from a list to proceed,
+  function is never called without a license present*/
 function renderLicenseLink(license) {
   return (license) ? `https://choosealicense.com/licenses/${license}/` : '';
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Create a function that returns the license section of README
+// STARTER COMMENT: If there is no license, return an empty string
+/* Validation is present in inquirer call, user selects from a list to proceed,
+  function is never called without a license present*/
+function renderLicenseSection(license) {
+  // Get the link urls
+  const link = renderLicenseLink(license);
+
+  // Return formatted license section of readme
+  return `## License
+
+    This project is covered under the [${license}](${link}) license.
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
