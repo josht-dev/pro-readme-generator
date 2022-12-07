f95dc218d48ea7633a6fb372e8f24513da94fddc
Joshua Taylor <toasterrage@gmail.com>
2022-12-06 22:16:59 -0700

    Added inquirer and fs modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Include packages needed for this application
-const genMd = require('./utils/generateMarkdown.js');
+const genMd = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
