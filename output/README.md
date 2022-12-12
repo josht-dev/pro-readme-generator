9720eea6a3132a3d034a60f43bdf616cb831805e
Joshua Taylor <toasterrage@gmail.com>
2022-12-11 17:12:47 -0700

    Modified multiline code

---
+++ output/README.md
@@ -0,0 +1,37 @@
+# TEST
+
+[![license](https://img.shields.io/badge/license-MIT-brightgreen?style=for-the-badge&logo=appveyor)](https://choosealicense.com/licenses/mit/)
+
+## Description
+
+test<br>    test<br>    test<br>    test
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A
+
+## Credits
+
+N/A
+
+## License
+
+This project is covered under the [MIT](https://choosealicense.com/licenses/mit/) license.
+
+## Questions
+
+For any questions, please reach out to one of my points of contact:
+[https://github.com/test](https://github.com/test) | test@cme.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,7 +40,7 @@ function renderLicenseSection(license) {
   // Get the link urls
   const link = renderLicenseLink(license);
   // Return formatted license section of readme
-  return (license) ? `<br><br>## License<br><br>This project is covered under the [${license}](${link}) license.` : '';
+  return (license) ? `\n\n## License\n\nThis project is covered under the [${license}](${link}) license.` : '';
 }
 
 // Create a function to generate markdown for README
@@ -56,13 +56,13 @@ function generateMarkdown(data) {
   // Check if the user indicated that the input had multiple lines with '//' in the input
   const multiLineCheck = (val) => {
     if (val.includes('//')) {
-      return val.replaceAll('//', '<br>');
+      return val.replaceAll('//', '<br>    ');
     } else {return val;}
   };
 
   // Did the user answer 'Y' to a table of contents
   if (data.index) {
-    tableContents = '<br><br>## Table of Contents<br>';
+    tableContents = '\n\n## Table of Contents\n';
     // Loop through the data obj dynamically adding a table of contents
     for (const i in data) {
       const x = i.toLowerCase();
@@ -73,27 +73,27 @@ function generateMarkdown(data) {
       // Check if data present to add to the table of contents
       if (data[i] || bareMin.includes(x)) {
         let link = x.replace(/ /g, '-');
-        tableContents = tableContents.concat(`<br>- [${i}](#${link})`);
+        tableContents = tableContents.concat(`\n- [${i}](#${link})`);
       } else {
         continue;
       }
     }
     // Add questions link
-    tableContents = tableContents.concat('<br>- [Questions](#questions)');
+    tableContents = tableContents.concat('\n- [Questions](#questions)');
   }
 
   // Loop through the data obj adding sections to the readme
   for (const key in data) {
     // Check if this is for the title
     if (key.toLowerCase() === 'title') {
-      readme = `# ${data[key].toUpperCase()}<br><br>${licBadge}`;
+      readme = `# ${data[key].toUpperCase()}\n\n${licBadge}`;
       continue;      
     }
     // Check if value is empty
     if (!data[key]) {
       // Check is this is a required section, otherwise skip
       if (bareMin.includes(key.toLowerCase())) {
-        readme = readme.concat(`<br><br>## ${key}<br><br>N/A`);
+        readme = readme.concat(`\n\n## ${key}\n\nN/A`);
         continue;
       } else {
         continue;
@@ -114,9 +114,9 @@ function generateMarkdown(data) {
     // Add the Questions section and skip the email obj key
     if (key.toLowerCase() === 'username') {
       readme = readme.concat(
-        '<br><br>## Questions<br><br>', 
+        '\n\n## Questions\n\n', 
         'For any questions, please reach out to one of my points of contact:', 
-        `<br>[https://github.com/${data[key]}](https://github.com/${data[key]})`, 
+        `\n[https://github.com/${data[key]}](https://github.com/${data[key]})`, 
         ` | ${data.email}`
       ); 
       continue;
@@ -125,7 +125,7 @@ function generateMarkdown(data) {
     
     // Handle all other sections that did not need specific logic
     data[key] = multiLineCheck(data[key]);
-    readme = readme.concat(`<br><br>## ${key}<br><br>${data[key]}`);
+    readme = readme.concat(`\n\n## ${key}\n\n${data[key]}`);
   }
 
   // Return formatted readme file
